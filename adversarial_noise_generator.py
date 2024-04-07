4f8d2185fb1e973913410d3367ce7c6a5e48de36
Han-IChun <falldancing@hotmail.com>
2024-04-07 22:40:40 +0100

    1.add closure fun in optimiser 2.add inverse transformer for noise mask for visuallisation

--- adversarial_noise_generator.py
+++ adversarial_noise_generator.py
@@ -1,4 +1,4 @@
-from torch.optim import Adam, SGD
+from torch.optim import Adam, SGD, LBFGS
 from torch.nn import CrossEntropyLoss
 import torch
 import requests
@@ -14,32 +14,42 @@ class TargetedAdvNoiseGenerator:
 
     def generate(self, image, original_class, target_class, max_iter=500):
         for i in range(max_iter):
-            # self.adversarial_noise = self.adversarial_noise.clamp(-0.001, 0.001)
-            perturbed_image = image + self.adversarial_noise
-            output = self.model(perturbed_image)
-            loss = CrossEntropyLoss()
-            loss_origin = -loss(output, torch.tensor([original_class]))
-            loss_target = loss(output, torch.tensor([target_class]))
-            
-            # showing progress
-            if i % 100 == 0:
-                print(f"Iteration {i}")
-                print("loss_origin: {}, loss_target: {}".format(loss_origin, loss_target))
+            def closure():
+                self.optimizer.zero_grad()
+                # self.adversarial_noise = torch.clip(self.adversarial_noise, -0.001, 0.001)
+                perturbed_image = image + self.adversarial_noise
+                output = self.model(perturbed_image)
+                loss = CrossEntropyLoss()
+                loss_origin = -loss(output, torch.tensor([original_class]))
+                loss_target = loss(output, torch.tensor([target_class]))
 
-            loss = loss_origin+loss_target
-            self.optimizer.zero_grad()
-            loss.backward()
-            self.optimizer.step()
+                loss = loss_target
+                torch.autograd.set_detect_anomaly(True)
+                loss.backward()
+                # showing progress
+                if i % 100 == 0:
+                    print(f"Iteration {i}")
+                    if i!=0:
+                        print("loss_origin: {}, loss_target: {}".format(loss_origin, loss_target))
+                    print(self.adversarial_noise.max(), self.adversarial_noise.min())
+                    print(self.adversarial_noise)
+                return loss
+            
+
+            self.optimizer.step(closure)
         return self.adversarial_noise
 
     def apply(self, image_url, target_class, max_iter=500, opt_lr = 5e-5):        
         # image processing
         response = requests.get(image_url)
         image = Image.open(BytesIO(response.content))
-        # these steps are based on the resnet50 model, need to generalize for other models in the future
+        # these steps are based on the resnet50 model, need to generalise for other models in the future
         resize_ratio = 232/min(image.size)
         image = image.resize((int(image.size[0]*resize_ratio),int(image.size[1]*resize_ratio) ), Image.BILINEAR)
         image = transforms.CenterCrop(224)(image)
+        # save the image for debugging
+        image.save("output_img/{}".format(image_url.split("/")[-1]))
+
         #  -----------------  #
 
         preprocess = self.weights.transforms()
@@ -50,19 +60,31 @@ class TargetedAdvNoiseGenerator:
 
         output = self.model(preprocessed_image).squeeze().softmax(dim=0)
         original_class = output.argmax().item()
+        print("original class: {}".format(original_class))
         # initiale adversarial noise corresponding to the image size
         self.adversarial_noise = torch.zeros((preprocessed_image.size(dim=0), 
                                               preprocessed_image.size(dim=1), 
                                               preprocessed_image.size(dim=2), 
                                               preprocessed_image.size(dim=3)), requires_grad=True)
-        self.optimizer = SGD([self.adversarial_noise], lr=opt_lr)
+        self.optimizer = Adam([self.adversarial_noise], lr=opt_lr)
         # generate adversarial noise
-        noise = self.generate(preprocessed_image, original_class, target_class, max_iter).squeeze(0).detach().numpy()
-        noise = noise.reshape(image.size[1], image.size[0], 3)
+        noise = self.generate(preprocessed_image, original_class, target_class, max_iter)
+        invTrans = transforms.Compose([ transforms.Normalize(mean = [ 0., 0., 0. ],
+                                                     std = [ 1/0.229, 1/0.224, 1/0.225 ]),
+                                transforms.Normalize(mean = [ -0.485, -0.456, -0.406 ],
+                                                     std = [ 1., 1., 1. ]),
+                               ])
+
+        noise = invTrans(noise)
+        noise = noise.squeeze(0).detach().numpy()
+        print("noise size: {}".format(noise.shape))
+        noise = noise.reshape(image.size[0], image.size[1], 3)
         output = np.array(image) + noise*255
         # turn output int into uint8
         output = np.array(output, dtype=np.uint8)
         output = Image.fromarray(output)
+        # save the image for debugging
+        output.save("output_img/{}".format(image_url.split("/")[-1].split(".")[0]+"_singleloss_altered."+image_url.split("/")[-1].split(".")[1])   )
         return output
     
 
